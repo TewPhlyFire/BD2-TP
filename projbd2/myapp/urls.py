a932ba3f580751e0487b02b8f7be702277ea6abe
Kuzuo-sama <100386818+Kuzuo-sama@users.noreply.github.com>
2024-12-24 01:46:40 +0000

    django

--- projbd2/myapp/urls.py
+++ projbd2/myapp/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('about-us/', views.about_us, name='about_us'),
+    path('', views.home_page_login, name='home_page_login'),
+    path('about_us/', views.about_us, name='about_us'),
     path('admin-page/', views.admin_page, name='admin_page'),
     path('adicionar-produto/', views.adicionar_produto, name='adicionar_produto'),
     path('apoio-cliente/', views.apoio_cliente, name='apoio_cliente'),
@@ -11,7 +12,6 @@ urlpatterns = [
     path('descobre-mais/', views.descobre_mais, name='descobre_mais'),
     path('detalhes-admin/', views.detalhes_admin, name='detalhes_admin'),
     path('folheto/', views.folheto, name='folheto'),
-    path('home-page-login/', views.home_page_login, name='home_page_login'),
     path('login/', views.login, name='login'),
     path('pagar-cenas/', views.pagar_cenas, name='pagar_cenas'),
     path('registrar/', views.registrar, name='registrar'),
